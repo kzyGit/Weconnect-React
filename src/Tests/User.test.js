10062895bec62fec47d3fb737491b3922679f495
KzyGit <kezzyangiro@gmail.com>
2018-07-29 23:12:14 +0300

    [Chore #159387346] Add User signup, login and logout tests

---
+++ src/Tests/User.test.js
@@ -0,0 +1,100 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+import Login from '../Components/Login';
+import Signup from '../Components/Signup';
+import Logout from '../Components/Logout';
+import { shallowToJson } from 'enzyme-to-json';
+
+
+describe('Login component', () => {
+
+    const localStorageMock = {
+        getItem: jest.fn(),
+        setItem: jest.fn(),
+        clear: jest.fn()
+    };
+    global.localStorage = localStorageMock;
+
+    const wrapper = shallow(<Login />);
+
+    it('renders properly', () => {
+        expect(shallowToJson(wrapper)).toMatchSnapshot();
+    });
+    it('contains Header', () => {
+        expect(wrapper.find('Header')).toHaveLength(1);
+    });
+    it('contains Footer', () => {
+        expect(wrapper.find('Footer')).toHaveLength(1);
+    });
+
+    it('contains divs', () => {
+        expect(wrapper.find('div')).toHaveLength(8);
+    });
+
+    it('contains headers', () => {
+        expect(wrapper.find('h3')).toHaveLength(1);
+    });
+
+    it('contains Forms', () => {
+        expect(wrapper.find('form')).toHaveLength(1);
+    });
+
+    it('contains button', () => {
+        expect(wrapper.find('button')).toHaveLength(1);
+    });
+    it('contains Link', () => {
+        expect(wrapper.find('Link')).toHaveLength(2);
+    });
+});
+
+
+describe('Signup component', () => {
+
+    const wrapper = shallow(<Signup />);
+
+    it('renders properly', () => {
+        expect(shallowToJson(wrapper)).toMatchSnapshot();
+    });
+    it('contains Header', () => {
+        expect(wrapper.find('Header')).toHaveLength(1);
+    });
+    it('contains Footer', () => {
+        expect(wrapper.find('Footer')).toHaveLength(1);
+    });
+
+    it('contains divs', () => {
+        expect(wrapper.find('div')).toHaveLength(10);
+    });
+
+    it('contains headers', () => {
+        expect(wrapper.find('h3')).toHaveLength(1);
+    });
+
+    it('contains Forms', () => {
+        expect(wrapper.find('form')).toHaveLength(1);
+    });
+
+    it('contains button', () => {
+        expect(wrapper.find('button')).toHaveLength(1);
+    });
+});
+
+describe('Logout component', () => {
+
+    const wrapper = shallow(<Logout />);
+
+    it('renders properly', () => {
+        expect(shallowToJson(wrapper)).toMatchSnapshot();
+    });
+    it('contains Header', () => {
+        expect(wrapper.find('Header')).toHaveLength(1);
+    });
+    it('contains Footer', () => {
+        expect(wrapper.find('Footer')).toHaveLength(1);
+    });
+
+    it('contains divs', () => {
+        expect(wrapper.find('div')).toHaveLength(1);
+    });
+
+});
