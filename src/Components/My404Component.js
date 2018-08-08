a7823a6d20315a37a859fbcc6be3efbb33600550
KzyGit <kezzyangiro@gmail.com>
2018-08-08 18:29:29 +0300

    [#159628288] Change 404component to a page rather than an alert,update my404component tests

--- src/Components/My404Component.js
+++ src/Components/My404Component.js
@@ -6,13 +6,18 @@ import { browserHistory } from 'react-router';
  * Component that enables '/' main route to have children routes
  */
 class My404Component extends Component {
-    componentDidMount() {
-        alert('Sorry, the page you are trying to access does not exist\n                             Go back to valid page');
+
+    redirect = (e) => {
         browserHistory.goBack();
     }
     render() {
         return (
-            <div className="row">
+            <div id="redirectpage">
+            <h1><i style={{color:'maroon'}} className="fa fa-exclamation-triangle" /></h1>
+                <h1 style={{color:'maroon'}}>Page Not Found</h1><br />
+                <p>The page you are trying to access does not exist. Go back to a valid page<br /><br /><br />
+                    <button  onClick={this.redirect} className="btn btn-default">Back</button>
+                </p><br />
             </div>
         );
     }
